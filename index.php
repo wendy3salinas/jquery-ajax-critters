4ec1710fa2ea5325370de7329a6b233b277d2746
Bill Newman <william.newman@seattlecolleges.edu>
2022-02-08 17:58:07 +0000

    Updates links on index

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<h1>jQuery AJAX Critters</h1>
+<html>
+  <head>
+    <title>jQuery AJAX Critters</title>
+  </head>
+  <body>
+      <h1>jQuery AJAX Critters</h1>
 
-<p>New stuff</p>
+      <p>Below are links to jQuery AJAX examples</p>
 
-<p>Stuff added from repo</p>
+      <p><a href="ja_load.html" target="_blank">ja_load.html</a></p>
+      <p><a href="ja_get.html" target="_blank">ja_get.html</a></p>
+      <p><a href="ja_post.html" target="_blank">ja_post.html</a></p>
+  </body>
+</html>
