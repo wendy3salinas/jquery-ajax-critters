afa9d45e626cf351a3f3758bcd9028015cc9c02b
Wendy Salinas-Reyes <wendy3salinas@gmail.com>
2022-02-10 04:40:10 +0000

    added new test

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 
       <p>Below are links to jQuery AJAX examples</p>
 
-      <p>This is a test<p>
+      <p>This is a test</p>
+       <p>This is a new test</p>
 
       <p><a href="ja_load.html" target="_blank">ja_load.html</a></p>
       <p><a href="ja_get.html" target="_blank">ja_get.html</a></p>
